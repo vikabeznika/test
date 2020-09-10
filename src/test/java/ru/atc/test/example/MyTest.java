e26f128f69194fa32a2a31f76059366e2e861580
VAntonova <vantonova@at-consulting.ru>
2020-09-10 11:47:00 +0300

    new changes

--- src/test/java/ru/atc/test/example/MyTest.java
+++ src/test/java/ru/atc/test/example/MyTest.java
@@ -3,6 +3,7 @@ package ru.atc.test.example;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -10,11 +11,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import java.util.concurrent.TimeUnit;
 
 public class MyTest {
     static protected WebDriver driver;
-
 
         /**
          * осуществление первоначальной настройки
@@ -24,23 +23,23 @@ public class MyTest {
             //определение пути до драйвера и его настройка
             System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
             //создание экземпляра драйвера
-            WebDriver driver = new ChromeDriver();
+            driver = new ChromeDriver();
             //окно разворачивается на полный экран
             driver.manage().window().maximize();
             //задержка на выполнение теста = 10 сек.
             driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
             //получение ссылки на страницу входа из файла настроек
-            driver.get(ConfProperties.getProperty("loginpage"));
-            //driver.get("C:\\Projects\\my-test\\src\\test\\resources\\helloWorldPage.html");
+            driver.get(ConfProperties.getProperty("page"));
         }
 
-
+        @AfterClass
+        public static void tearDown() {
+            driver.quit();
+        }
 
     @Test
-    public void loginTest() {
-    String text1 = "Hello, World!";
-    String text = "Войдите, чтобы продолжить";
-    System.out.println("hello");
+    public void Test() {
+    String text = "Hello, World!";
         List<WebElement> list = driver.findElements(By.xpath("//*[contains(text(),'" + text + "')]"));
         Assert.assertTrue(text, list.size() > 0);
     }
---
+++ src/test/java/ru/atc/test/example/TestSuite.java
@@ -0,0 +1,13 @@
+package ru.atc.test.example;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        MyTest.class
+} )
+
+public class TestSuite {
+
+}
